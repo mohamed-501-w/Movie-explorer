fe741f960b5e64d0e9adbedb6467f217e3917312
mohamed-501-w <mohamedwaled501@gmail.com>
2026-07-08 06:39:01 +0300

    nav done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,21 +4,24 @@ export default function Nav() {
 
 return (
     <>
-        <nav className="flex border-b py-5 px-8 justify-between">
-            <form className="flex-1 flex max-w-1/2 drop-shadow-secondary drop-shadow-sm  rounded-full" action="">
+        <nav className="flex border-b border-gray-500 shadow shadow-white/45 py-5 px-8 justify-evenly gap-x-2.5 font-semibold">
+            <form className="flex-1 flex max-w-1/2 drop-shadow-secondary drop-shadow-sm  rounded-full" 
+
+            action="">
+
                 <input 
-                className="flex-1 px-5 py-1 rounded-l-full outline-0 text-primary bg-secondary placeholder:text-primary" 
-                placeholder="EndGame" 
+                className="flex-1 px-5 py-1 rounded-l-full outline-0 text-primary bg-secondary placeholder:text-primary/50 text-lg" 
+                placeholder="Search" 
                 type="text" 
                 name="search"/>
+
                 <button
-                className="text-secondary bg-primary pl-7 pr-5 rounded-r-full"
+                className="peer text-secondary bg-primary pl-7 pr-5 py-2 rounded-r-full transition-colors hover:bg-hover hover:text-primary hover:drop-shadow-amber-950 hover:drop-shadow-sm"
                 >
-                    <svg className=""
-                    
+                    <svg className="size-6"
+                    width="24"
+                    height="24"
                     xmlns="http://www.w3.org/2000/svg" 
-                    width="24" 
-                    height="24" 
                     fill="currentColor" 
                     class="bi bi-search" 
                     viewBox="0 0 16 16">
@@ -26,10 +29,13 @@ return (
                     </svg>
                 </button>
             </form>
-                <div>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/Movie">Trending</NavLink>
-                    <NavLink to="/Favorites">All Concerts</NavLink>
+                <div className="flex gap-4 text-2xl italic px-2 items-center text-main tracking-widest text-shadow-secondary text-shadow-xs">
+                    <NavLink
+                    className="px-2.5 py-2 transition-all hover:scale-110 hover:text-hover" 
+                    to="/">Home</NavLink>
+                    <NavLink 
+                    className="px-2.5 py-2 transition-all hover:scale-110 hover:text-hover"
+                    to="/Favorites"> Favorites</NavLink>
                 </div>
         </nav>
         <Outlet />
