9e4f9e5836cffa6c91d956104994492f38aed4ea
mohamed-501-w <mohamedwaled501@gmail.com>
2026-07-09 21:08:54 +0300

    fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,49 @@ import {data} from '../data/dummyData'
 
 function Home() {
   const [trendingList, setTrendingList] = useState(data.results)
-
+  const [popularList, setPopularList] = useState([])
   const [heroIndex,setHeroIndex] = useState(0)
 
+
+  // useEffect( ()=> {
+  //   const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       accept: 'application/json',
+  //       Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`
+  //     }                          
+  //   };
+
+  //   fetch(url, options)
+  //     .then(res => res.json())
+  //     .then(json => setTrendingList(json.results))
+  //     .catch(err => console.error(err));
+
+
+  //     const urlPop = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+  //     const optionsPop = {
+  //       method: 'GET',
+  //       headers: {accept: 'application/json', Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`}
+  //     };
+
+  //     fetch(urlPop, options)
+  //       .then(res => res.json())
+  //       .then(json => setPopularList(json.results))
+  //       .catch(err => console.error(err));
+
+
+  //   }, [])
+
   useEffect(()=>{
-    setInterval(()=>{
+    const timer = setInterval(()=>{
     setHeroIndex(prev => (prev + 1 ) % trendingList.length) 
   }, 7010)
+  return () => {
+    clearInterval(timer)
+  }
+
   },[])
 
   return (
@@ -38,7 +74,7 @@ function Home() {
       {/* section popular */}
         
       <MovieCardsContainer 
-      movies={trendingList}
+      movies={popularList}
       title="Popular: "/>
     </>
   )
