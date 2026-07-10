a34097f2c0f85a1be156022776d92caa32a004aa
mohamed-501-w <mohamedwaled501@gmail.com>
2026-07-11 00:33:28 +0300

    add animation for movie page add movie hero and imgs to page

--- src/components/MovieHero.jsx
+++ src/components/MovieHero.jsx
@@ -5,20 +5,20 @@ import { Link } from "react-router";
 export default function MovieHero({movie, isHome, isMovie ,isE}){
     return (
         <Link to={`/movie/${movie.id}`} 
-        className={clsx("flex justify-center items-center shadow shadow-gr mb-10", isHome &&"animate-fadeLeft ", isMovie &&"")}>
+        className={clsx("flex flex-col-reverse px-10 md:flex-row justify-center items-center shadow shadow-gr mb-5", isHome &&"animate-fadeLeft ", isMovie &&"animate-fadeTop")}>
             <div className="pl-4 max-w-md shadow "> 
-                <h1 className="text-3xl mb-10 font-black tracking-wider italic text-shadow-lg/20 text-shadow-secondary ">
+                <h1 className="text-2xl md:text-3xl mb-10 font-black tracking-wider italic text-shadow-lg/20 text-shadow-secondary ">
                     {movie.title}
                 </h1>
 
                 <p 
-                className="text-lg font-semibold italic opacity-70 tracking-wide">
+                className=" md:text-lg font-semibold italic opacity-70 tracking-wide">
                     {movie.overview}
                 </p>
 
             </div>
             <img 
-            className={clsx("rounded-sm  mask-radial-from-30% mask-radial-to-95% mask-radial-at-right mask-l-from-50 mask-r-from-95% mask-b-from-98% ", isMovie && "h-150" )}
+            className={clsx("rounded-sm  mask-radial-from-30% mask-radial-to-95%  mask-l-from-50 mask-r-from-95% mask-b-from-98% ", isMovie && "w-90" )}
             src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
         </Link>
     )
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,29 +1,43 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import MovieHero from "../components/MovieHero";
-
-
+import MovieDetails from "../components/MovieDetails";
+import MovieImages from "../components/MovieImages";
 
 
 
 
 export default function Movie() {
     const [movie, setMovie] = useState({})
+    const [images, setImages] = useState([])
+
     let { id } = useParams();
 
     useEffect( ()=> {
     const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
 
-    const options = {
+    const movieOptions = {
         method: 'GET',
         headers: {
         accept: 'application/json',
         Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`
         }                          
     };
-    fetch(url,options)
+    fetch(url,movieOptions)
         .then(res => res.json())
         .then(res => setMovie(res))
+        .catch(err => console.error(err));
+
+
+
+    const imagesOptions = {method: 'GET', headers: {accept: 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`
+    }};
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}/images`, imagesOptions)
+    .then(res => res.json())
+    .then(res => setImages(res.backdrops))
+    .catch(err => console.error(err));
 
 
     }, [id])
@@ -35,6 +49,14 @@ return (
         movie={movie} 
         isHome={false} 
         isMovie={true}/>
+        
+        <MovieImages 
+        images={images}
+        />
+
+        <MovieDetails 
+        movie={movie}
+        />
     </>
 )
 }
