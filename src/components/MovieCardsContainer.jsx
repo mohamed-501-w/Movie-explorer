0967a851fd10c28ae3ddf06f6c7c7b5cdc0fdf18
mohamed-501-w <mohamedwaled501@gmail.com>
2026-07-11 00:32:23 +0300

    fixed title placement bug

--- src/components/MovieCardsContainer.jsx
+++ src/components/MovieCardsContainer.jsx
@@ -13,9 +13,8 @@ export default function MovieCardsContainer({movies ,title}){
 
     return (
         <>
-
-            <section className="snap-x overflow-x-clip mx-4 my-8">
-                <h3 className="text-xl font-bold mb-4 text-shadow-secondary text-shadow-xs tracking-wider">{title}</h3>
+             <h3 className="text-xl font-bold mb-4 text-shadow-secondary text-shadow-xs tracking-wider">{title}</h3>
+            <section className="snap-x overflow-x-scroll  custom-scrollbar  mx-4 my-8">
                 <section className=" flex items-stretch ">
                     {moviesEl}
                 </section>
